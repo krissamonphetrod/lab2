c82c41c9c2fd3cb7e28c1d0737b1b9e9f545676c
OX <59160926@go.buu.ac.th>
2018-09-01 19:24:27 +0700

    Finish before checkWin

---
+++ src/Game.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Game {
+    private static OX ox;
+    private  static Scanner sc = new Scanner(System.in);
+    private static int col;
+    private static int row;
+
+    public static void main(String[] args) {
+        ox = new OX();
+        while (true) {
+            printTable();
+            input();
+
+            ox.switchPlayer();
+
+        }
+    }
+
+    private static void input() {
+        boolean canPut = true;
+        do {
+            System.out.println(ox.gerCurrentPlayer() + " Col :");
+            col = sc.nextInt();
+            System.out.println(ox.gerCurrentPlayer() + " Row :");
+            row = sc.nextInt();
+            canPut = ox.put(col, row);
+            if (!canPut) {
+                System.out.println("Please input number between 0-2");
+            }
+        } while (!canPut);
+
+    }
+
+    private static void printTable() {
+        System.out.print(ox.getTableString());
+    }
+}
